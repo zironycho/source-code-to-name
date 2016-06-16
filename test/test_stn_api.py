eeaf14eb547d0cb68aa9d7ef2c94eaad532ea295
juncheol <juncheol@qti.qualcomm.com>
2016-06-16 18:24:38 +0900

    init test:

---
+++ test/test_stn_api.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class TestStnApi(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
+    def test_will_failed(self):
+        self.assertTrue(False)
+
