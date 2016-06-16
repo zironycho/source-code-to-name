5ec4a83558b481ee48da950fbb2ddee959acc3b0
Juncheol Cho <ziron1397@gmail.com>
2016-06-17 02:27:42 +0900

    add first api: getname()

---
+++ codetoname/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def getname(code):
+	if not code:
+		return False
+
+	return 'get_a'
---
+++ test/test_api.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import codetoname
+
+
+class TestApi(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
+    def test_getname_with_empty_code(self):
+        self.assertFalse(codetoname.getname(''))
+
+    def test_getname(self):
+
+        code_block =\
+            """
+            def unknown_name():
+                return a
+            """
+        self.assertEqual('get_a', codetoname.getname(code_block))
