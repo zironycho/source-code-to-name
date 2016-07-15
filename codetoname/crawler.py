713546be0b543d817385617fa5d13ef3390dd0ab
Juncheol Cho <ziron1397@gmail.com>
2016-07-15 21:26:17 +0900

    check exist github.id from nested objects move fetch function into the class

--- codetoname/crawler.py
+++ codetoname/crawler.py
@@ -1,35 +1,30 @@
 # -*- coding: utf-8 -*-
 import json
-
+import github
 import elasticsearch
 import elasticsearch_dsl
-import github
-from elasticsearch_dsl.aggs import A
 
+from elasticsearch_dsl.aggs import A
 from codetoname.features import from_repo
 
 
-def fetch_github_repos(language, repo_size=10, repo_page=0, account=None, password=None):
-    start_offset = repo_size * repo_page
-    end_offset = start_offset + repo_size
-
-    if account and password:
-        github_client = github.Github(account, password)
-    else:
-        github_client = github.Github()
-    repos = github_client.search_repositories(query='language:{}'.format(language.strip()))
-    repos = repos[start_offset:end_offset]
-    return [{'url': r.clone_url, 'branch': r.default_branch, 'github_id': r.id, 'fork': r.fork} for r in repos]
-
-
 class Crawler:
-    def __init__(self, index='codetoname', page_num=0, page_size=10, account=None, password=None):
+    def __init__(self, index='codetoname', page_num=0, page_size=10,
+                 language='python', account=None, password=None):
         self._es = elasticsearch.Elasticsearch()
         self._es_index = index
         self._page_num = page_num
         self._page_size = page_size
-        self._account = account
-        self._password = password
+        self._language = language
+        if account and password:
+            github_client = github.Github(account, password)
+        else:
+            github_client = github.Github()
+
+        self._github_response = github_client.search_repositories(
+            query='language:{}'.format(self._language.strip()))
+
+        self._latest_repo_index = False
 
     def delete_index(self):
         if self._es.indices.exists(index=self._es_index):
@@ -39,47 +34,72 @@ class Crawler:
         if not self._es.indices.exists(index=self._es_index):
             self._es.indices.create(index=self._es_index)
 
+    def fetch_github_repos(self, page_size=None):
+        # update page size
+        if page_size:
+            self._page_size = page_size
+
+        # calculate new start-end indices
+        if self._latest_repo_index:
+            start_index = self._latest_repo_index
+        else:
+            start_index = self._page_size * self._page_num
+        end_index = start_index + self._page_size
+
+        # update latest index, and page num
+        self._latest_repo_index = end_index
+        self._page_num += 1
+
+        # fetch
+        repos = self._github_response[start_index:end_index]
+        return [{'url': r.clone_url, 'branch': r.default_branch, 'github_id': r.id, 'fork': r.fork} for r in repos]
+
     def next(self):
         self.create_index()
-        language = 'python'
-        repos = fetch_github_repos(language=language,
-                                   repo_size=self._page_size, repo_page=self._page_num,
-                                   account=self._account, password=self._password)
-        for repo in repos:
-            if 0 != elasticsearch_dsl\
-                    .Search(using=self._es, index=self._es_index, doc_type=language)\
-                    .query('term', github_id=repo['github_id'])\
-                    .count():
-                continue
+        for repo in self.fetch_github_repos():
+            if not self.exists_repos_in_database(repo['github_id']):
+                try:
+                    features = from_repo(repo, language=self._language)
+                    for f in features:
+                        self._es.index(
+                            index=self._es_index,
+                            doc_type=self._language,
+                            body={'repo': repo, 'feature': json.dumps(f)}
+                        )
+                    if not features:
+                        self._es.index(
+                            index=self._es_index,
+                            doc_type=self._language,
+                            body={'repo': repo})
+                    self._es.indices.refresh(index=self._es_index)
+                except Exception as e:
+                    print(repo)
+                    print(f) if f else None
+                    print(e)
 
-            try:
-                features = from_repo(repo, language=language)
-                for f in features:
-                    self._es.index(
-                        index=self._es_index,
-                        doc_type=language,
-                        body={'repo': repo, 'feature': json.dumps(f)}
-                    )
-                if not features:
-                    self._es.index(
-                        index=self._es_index,
-                        doc_type=language,
-                        body={'repo': repo})
-                self._es.indices.refresh(index=self._es_index)
-            except Exception as e:
-                print(repo)
-                print(f) if f else None
-                print(e)
-
-        self._page_num += 1
+    def exists_repos_in_database(self, github_id):
+        if 0 != elasticsearch_dsl \
+                .Search(using=self._es, index=self._es_index, doc_type=self._language) \
+                .query('term', repo__github_id=github_id) \
+                .count():
+            return True
+        return False
 
     def num_features(self):
         return self._es.count(index=self._es_index)['count']
 
-    def num_repos(self, language):
+    def num_repos(self):
         if self._es.indices.exists(index=self._es_index):
-            s = elasticsearch_dsl.Search(using=self._es, index=self._es_index, doc_type=language)
+            s = elasticsearch_dsl.Search(using=self._es, index=self._es_index, doc_type=self._language)
             s.aggs.bucket('num_repos', A('cardinality', field='repo.github_id'))
             response = s.execute()
             return response.aggregations.num_repos.value
         return 0
+
+    def get_features(self):
+        if self._es.indices.exists(index=self._es_index):
+            s = elasticsearch_dsl.Search(using=self._es, index=self._es_index, doc_type=self._language)
+            response = s.execute()
+            if 0 != len(response.hits):
+                return response.hits
+        return False
--- test/test_crawler.py
+++ test/test_crawler.py
@@ -11,7 +11,7 @@ class TestCrawler(unittest.TestCase):
         self.crawler.delete_index()
 
     def test_fetch_github_repos(self):
-        repos = codetoname.crawler.fetch_github_repos('python', repo_page=0)
+        repos = self.crawler.fetch_github_repos(page_size=10)
         self.assertEqual(10, len(repos))
         self.assertIn('url', repos[0])
         self.assertIn('branch', repos[0])
@@ -19,7 +19,8 @@ class TestCrawler(unittest.TestCase):
         self.assertIn('fork', repos[0])
         self.assertIn('https://github.com/jkbrzt/httpie.git', [r['url'] for r in repos])
 
-        next_repos = codetoname.crawler.fetch_github_repos('python', repo_page=1, repo_size=15)
+        next_repos = self.crawler.fetch_github_repos()
+        self.assertEqual(10, len(next_repos))
 
         the_same = True
         for u in next_repos:
@@ -38,6 +39,14 @@ class TestCrawler(unittest.TestCase):
 
     def test_num_repos(self):
         self.crawler.next()
-        self.assertEqual(1, self.crawler.num_repos('python'))
+        self.assertEqual(1, self.crawler.num_repos())
         self.crawler.next()
-        self.assertEqual(2, self.crawler.num_repos('python'))
+        self.assertEqual(2, self.crawler.num_repos())
+
+    def test_exist_repos(self):
+        self.crawler.next()
+        features = self.crawler.get_features()
+        self.assertTrue(features)
+        github_id = features[0]['repo']['github_id']
+        print(github_id)
+        self.assertTrue(self.crawler.exists_repos_in_database(github_id))
